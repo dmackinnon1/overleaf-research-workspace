7c7935100b762fea365245d29b6af1a2e93d4dc6
Dan MacKinnon <dmackinnon1@gmail.com>
2025-11-29 22:03:21 -0500

    Update generatePlots.r

--- monty-hall/generatePlots.r
+++ monty-hall/generatePlots.r
@@ -1,12 +1,12 @@
-MontyHall2 <- read.table("output_data.txt", header = TRUE, sep = "\t");
+MontyHall2 <- read.table("./monty-hall/output_data.txt", header = TRUE, sep = "\t");
 
-png("barplot_output.png", width = 800, height = 600, res = 100);
+png("./monty-hall/barplot_output.png", width = 800, height = 600, res = 100);
 barplot(table(MontyHall2$prizeDoor),xlab="Door");
 
 dev.off()
 
 t <- table(MontyHall2$outcome, MontyHall2$action)
-png("barplot2_output.png", width = 800, height = 600, res = 100);
+png("/monty-hall/barplot2_output.png", width = 800, height = 600, res = 100);
 
 barplot(t,
         xlab="wins", col=c("grey","black"),legend.text = TRUE, 
